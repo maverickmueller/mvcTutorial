80cd078eca2cfcb8c4df509131b489c7b0f1833f
Caleb <maverickmueller@gmail.com>
2016-05-06 09:44:00 -0500

    It's now a really ugly yellow

--- mvcTutorial/mvcTutorial/Global.asax.cs
+++ mvcTutorial/mvcTutorial/Global.asax.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using mvcTutorial.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace mvcTutorial
 {
@@ -13,6 +15,7 @@ namespace mvcTutorial
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
